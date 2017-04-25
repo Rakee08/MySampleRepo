3327b3c52bfea3bfa72e47d8e756488e01223373
Ganesh <ganesh@supremetechnologies.in>
2017-04-25 16:45:21 +0530

    Initial Commit

---
+++ SampleRepo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  SampleRepo
+//
+//  Created by Srinivasa Ganesan on 25/04/17.
+//  Copyright © 2017 SupremeTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ SampleRepo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SampleRepo
+//
+//  Created by Srinivasa Ganesan on 25/04/17.
+//  Copyright © 2017 SupremeTechnologies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
